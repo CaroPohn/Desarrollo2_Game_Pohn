982321d2e6d96cf2408c2d9e5eaffe05731da98f
Caro <carolinapohn@gmail.com>
2024-06-30 06:38:04 -0300

    Add additive scene system and menu navigation

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,35 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public enum scenes
+    [Header("Main Menu Canvas")]
+    [SerializeField] private GameObject mainMenuCanvas;
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button controlsButton;
+    [SerializeField] private Button exitButton;
+
+    [Header("Credits Canvas")]
+    [SerializeField] private GameObject creditsCanvas;
+    [SerializeField] private Button creditsBackButton;
+
+    [Header("Controls Canvas")]
+    [SerializeField] private GameObject controlsCanvas;
+    [SerializeField] private Button controlsBackButton;
+
+    private GameObject currentCanvas;
+
+    private void Awake()
     {
-        MainMenu,
-        Credits,
-        Controls,
-        InitialLevel,
+        playButton.onClick.AddListener(StartPlayScene);
+        creditsButton.onClick.AddListener(GoToCreditsCanvas);
+        controlsButton.onClick.AddListener(GoToControlsCanvas);
+        exitButton.onClick.AddListener(QuitGame);
+
+        creditsBackButton.onClick.AddListener(GoToMenuCanvas);
+        controlsBackButton.onClick.AddListener(GoToMenuCanvas);
     }
 
-    public void Play()
+
+    private void Start()
     {
-        SceneManager.LoadScene((int)scenes.InitialLevel);
+        ChangeCurrentCanvas(mainMenuCanvas, playButton.gameObject);
     }
 
-    public void Credits()
+    private void OnDestroy()
     {
-        SceneManager.LoadScene((int)scenes.Credits);
+        playButton.onClick.RemoveListener(StartPlayScene);
+        creditsButton.onClick.RemoveListener(GoToCreditsCanvas);
+        controlsButton.onClick.RemoveListener(GoToControlsCanvas);
+        exitButton.onClick.RemoveListener(QuitGame);
+
+        creditsBackButton.onClick.RemoveListener(GoToMenuCanvas);
+        controlsBackButton.onClick.RemoveListener(GoToMenuCanvas);
     }
 
-    public void Controls()
+    private void StartPlayScene()
     {
-        SceneManager.LoadScene((int)scenes.Controls);
+        SceneLoader.Instance.ChangeScene("Tutorial");
     }
 
-    public void Quit()
+    private void GoToCreditsCanvas()
+    {
+        ChangeCurrentCanvas(creditsCanvas, creditsBackButton.gameObject);
+    }
+
+    private void GoToControlsCanvas()
+    {
+        ChangeCurrentCanvas(controlsCanvas, controlsBackButton.gameObject);
+    }
+
+    private void QuitGame()
     {
 #if UNITY_EDITOR
         if (UnityEditor.EditorApplication.isPlaying)
@@ -38,5 +76,21 @@ public class MainMenu : MonoBehaviour
             }
 #endif
         Application.Quit();
+    }
+
+    private void GoToMenuCanvas()
+    {
+        ChangeCurrentCanvas(mainMenuCanvas, playButton.gameObject);
+    }
+
+    private void ChangeCurrentCanvas(GameObject newCanvas, GameObject selectedGameObject)
+    {
+        if(currentCanvas != null)
+            currentCanvas.SetActive(false);
+
+        newCanvas.SetActive(true);
+
+        currentCanvas = newCanvas;
+        EventSystem.current.SetSelectedGameObject(selectedGameObject);
     }
 }
